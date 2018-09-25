bcc9220a0f2ae7cf64128eea9ad9441398ca731a
Santosh Akkinapalli <satyasantosh.akkinapalli@gmail.com>
2018-09-26 02:08:41 +0530

    all the fields added

--- src/main/java/com/synergy/recupro/model/Accounts.java
+++ src/main/java/com/synergy/recupro/model/Accounts.java
@@ -1,5 +1,6 @@
 package com.synergy.recupro.model;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -7,16 +8,20 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 
 @Entity
-@Table(name = "accounts")
+@Table(name = "ACCOUNTS")
 public class Accounts extends AuditModel {
 	
     /**
@@ -31,10 +36,436 @@ public class Accounts extends AuditModel {
             initialValue = 1000
     )
     private Long id;
-    @Column(columnDefinition = "text")
-    private String name;
-    @Column(columnDefinition = "text")
-    private String type;
+	
+    @Column(columnDefinition = "text", nullable = false, name = "ACCOUNT_NAME")
+    @Size(min = 3, max = 25)
+    private String accountName;
+    
+    @Column(columnDefinition = "text", name = "CONTACT_NAME")
+    @Size(min = 3, max = 35)
+    private String contactName;
+    
+    @Column(columnDefinition = "text", nullable = false, name = "OPENING_TITLE")
+    @Size(min = 3, max = 25)
+    private String openingTitle;
+    
+    @Column(columnDefinition = "text", nullable = false, name = "OPENING_ID")
+    private String openingId;
+    
+    @Column(columnDefinition = "text", name = "ACCOUNT_OWNER")
+    @Size(min = 3, max = 35)
+    private String accountOwner;
+    
+    @Column(columnDefinition = "text", name = "PRIMARY_RECRUITER")
+    @Size(min = 3, max = 35)
+    private String primaryRecruiter;
+    
+    @Column(columnDefinition = "text", name = "ACCESS")
+    @Size(min = 3, max = 15)
+    private String access;
+    
+    @Column(columnDefinition = "text", name = "ASGN_MRE_RECRUITERS")
+    @Size(min = 3, max = 35)
+    private String assignMoreRecruiter;
+    
+    @Column(columnDefinition = "text", name = "END_CLIENT")
+    @Size(min = 3, max = 15)
+    private String endClient;
+    
+    @Column(columnDefinition = "int", name = "REQ_EXP")
+    private String requiredExperience;
+    
+    @Column(columnDefinition = "int", name = "BILL_RATE")
+    private int billRate;
+    
+    @Column(columnDefinition = "int", name = "PAY_RATE")
+    private String payRate;
+    
+    @Column(columnDefinition = "text", name = "COUNTRY")
+    @Size(max = 75)
+    private String country;
+    
+    @Column(columnDefinition = "text", name = "STATE")
+    @Size(max = 75)
+    private String state;
+    
+    @Column(columnDefinition = "text", name = "CITY")
+    @Size(max = 75)
+    private String city;
+    
+    @Column(columnDefinition = "int", name = "ZIP_CODE")
+    private int zipCode;
+    
+    @Column(columnDefinition = "int", name = "NO_OF_OPENINGS")
+    @Size(max = 6)
+    private long numberOfOpenings;
+    
+    @Column(columnDefinition = "int", name = "MAX_RES_ALLOWED")
+    @Size(max = 6)
+    private long maxResumesAllowed;
+    
+    @Column(columnDefinition = "boolean", name = "LOCAL_INDICATOR")
+    private boolean localIndicator;
+    
+    @Column(columnDefinition = "text", nullable = false, name = "BRIEF_DESCRIPTION")
+    @Size(min = 10, max = 1000)
+    private String briefDescription;
+    
+    @Column(columnDefinition = "text", name = "DESCRIPTION")
+    @Size(min = 10, max = 1000)
+    private String description;
+    
+    @Column(columnDefinition = "int", name = "DURATION")
+    private int duration;
+    
+    @Column(columnDefinition = "text", name = "DUR_CATEGORY")
+    @Size(max = 25)
+    private String durationCategory;
+    
+    @Column(columnDefinition = "text", name = "DUR_SUB_CATEGORY")
+    @Size(max = 25)
+    private String durationSubCategory;
+    
+    @Column(columnDefinition = "text", name = "DUR_EMP_TYPE")
+    @Size(max = 25)
+    private String durationEmpType;
+    
+    @Column(columnDefinition = "text", name = "DUR_STATUS")
+    @Size(max = 25)
+    private String durationStatus;
+    
+    @Column(columnDefinition = "text", name = "DUR_EXPR_LEVEL")
+    @Size(max = 25)
+    private String durationExperienceLevel;
+    
+    @Column(columnDefinition = "text", name = "DUR_POS_TYPE")
+    @Size(max = 25)
+    private String durationPositionType;
+    
+    @Column(columnDefinition = "text", name = "DUR_INTERVIEW_TYPE")
+    @Size(max = 25)
+    private String durationInterviewType;
+    
+    @Column(columnDefinition = "text", name = "DUR_VISA_TYPE")
+    @Size(max = 25)
+    private String durationVisaType;
+    
+    @Temporal(TemporalType.DATE)
+    @Column(name = "DUR_PROJ_START_DATE")
+    private Date durationProjectStartDate;
+    
+    @Temporal(TemporalType.DATE)
+    @Column(name = "DUR_PROJ_CLOSE_DATE")
+    private Date durationProjectCloseDate;
+    
+    @Column(columnDefinition = "text", name = "NOTES")
+    @Size(min = 10, max = 500)
+    private String notes;
+    
+    @Lob
+    @Column(columnDefinition = "text", name = "ATTACHMENTS")
+    private byte[] attachment;
+    
+    @Column(columnDefinition = "boolean", name = "ATTACH_MAKE_DEFAULT")
+    private boolean localAttchMakeDefault;
+    
+    public String getBriefDescription() {
+		return briefDescription;
+	}
+
+	public void setBriefDescription(String briefDescription) {
+		this.briefDescription = briefDescription;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public String getDurationCategory() {
+		return durationCategory;
+	}
+
+	public void setDurationCategory(String durationCategory) {
+		this.durationCategory = durationCategory;
+	}
+
+	public String getDurationSubCategory() {
+		return durationSubCategory;
+	}
+
+	public void setDurationSubCategory(String durationSubCategory) {
+		this.durationSubCategory = durationSubCategory;
+	}
+
+	public String getDurationEmpType() {
+		return durationEmpType;
+	}
+
+	public void setDurationEmpType(String durationEmpType) {
+		this.durationEmpType = durationEmpType;
+	}
+
+	public String getDurationStatus() {
+		return durationStatus;
+	}
+
+	public void setDurationStatus(String durationStatus) {
+		this.durationStatus = durationStatus;
+	}
+
+	public String getDurationExperienceLevel() {
+		return durationExperienceLevel;
+	}
+
+	public void setDurationExperienceLevel(String durationExperienceLevel) {
+		this.durationExperienceLevel = durationExperienceLevel;
+	}
+
+	public String getDurationPositionType() {
+		return durationPositionType;
+	}
+
+	public void setDurationPositionType(String durationPositionType) {
+		this.durationPositionType = durationPositionType;
+	}
+
+	public String getDurationInterviewType() {
+		return durationInterviewType;
+	}
+
+	public void setDurationInterviewType(String durationInterviewType) {
+		this.durationInterviewType = durationInterviewType;
+	}
+
+	public String getDurationVisaType() {
+		return durationVisaType;
+	}
+
+	public void setDurationVisaType(String durationVisaType) {
+		this.durationVisaType = durationVisaType;
+	}
+
+	public Date getDurationProjectStartDate() {
+		return durationProjectStartDate;
+	}
+
+	public void setDurationProjectStartDate(Date durationProjectStartDate) {
+		this.durationProjectStartDate = durationProjectStartDate;
+	}
+
+	public Date getDurationProjectCloseDate() {
+		return durationProjectCloseDate;
+	}
+
+	public void setDurationProjectCloseDate(Date durationProjectCloseDate) {
+		this.durationProjectCloseDate = durationProjectCloseDate;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	public byte[] getAttachment() {
+		return attachment;
+	}
+
+	public void setAttachment(byte[] attachment) {
+		this.attachment = attachment;
+	}
+
+	public boolean isLocalAttchMakeDefault() {
+		return localAttchMakeDefault;
+	}
+
+	public void setLocalAttchMakeDefault(boolean localAttchMakeDefault) {
+		this.localAttchMakeDefault = localAttchMakeDefault;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	//-------------------------------------------------
+    		//DELETE FROM HERE TILL END
+    //-------------------------------------------------
+    public String getAccountName() {
+		return accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+
+	public String getContactName() {
+		return contactName;
+	}
+
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+
+	public String getOpeningTitle() {
+		return openingTitle;
+	}
+
+	public void setOpeningTitle(String openingTitle) {
+		this.openingTitle = openingTitle;
+	}
+
+	public String getOpeningId() {
+		return openingId;
+	}
+
+	public void setOpeningId(String openingId) {
+		this.openingId = openingId;
+	}
+
+	public String getAccountOwner() {
+		return accountOwner;
+	}
+
+	public void setAccountOwner(String accountOwner) {
+		this.accountOwner = accountOwner;
+	}
+
+	public String getPrimaryRecruiter() {
+		return primaryRecruiter;
+	}
+
+	public void setPrimaryRecruiter(String primaryRecruiter) {
+		this.primaryRecruiter = primaryRecruiter;
+	}
+
+	public String getAccess() {
+		return access;
+	}
+
+	public void setAccess(String access) {
+		this.access = access;
+	}
+
+	public String getAssignMoreRecruiter() {
+		return assignMoreRecruiter;
+	}
+
+	public void setAssignMoreRecruiter(String assignMoreRecruiter) {
+		this.assignMoreRecruiter = assignMoreRecruiter;
+	}
+
+	public String getEndClient() {
+		return endClient;
+	}
+
+	public void setEndClient(String endClient) {
+		this.endClient = endClient;
+	}
+
+	public String getRequiredExperience() {
+		return requiredExperience;
+	}
+
+	public void setRequiredExperience(String requiredExperience) {
+		this.requiredExperience = requiredExperience;
+	}
+
+	public int getBillRate() {
+		return billRate;
+	}
+
+	public void setBillRate(int billRate) {
+		this.billRate = billRate;
+	}
+
+	public String getPayRate() {
+		return payRate;
+	}
+
+	public void setPayRate(String payRate) {
+		this.payRate = payRate;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public long getNumberOfOpenings() {
+		return numberOfOpenings;
+	}
+
+	public void setNumberOfOpenings(long numberOfOpenings) {
+		this.numberOfOpenings = numberOfOpenings;
+	}
+
+	public long getMaxResumesAllowed() {
+		return maxResumesAllowed;
+	}
+
+	public void setMaxResumesAllowed(long maxResumesAllowed) {
+		this.maxResumesAllowed = maxResumesAllowed;
+	}
+
+	public boolean isLocalIndicator() {
+		return localIndicator;
+	}
+
+	public void setLocalIndicator(boolean localIndicator) {
+		this.localIndicator = localIndicator;
+	}
+
+	private String type;
     @Column(columnDefinition = "text")
     private String address;
     @Column(columnDefinition = "text")
@@ -57,11 +488,11 @@ public class Accounts extends AuditModel {
 	}
 
 	public String getName() {
-		return name;
+		return accountName;
 	}
 
 	public void setName(String name) {
-		this.name = name;
+		this.accountName = name;
 	}
 
 	public String getType() {
